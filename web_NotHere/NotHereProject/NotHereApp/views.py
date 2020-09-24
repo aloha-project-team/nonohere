d4b9fca3c685e6c4ac7397e84d2de907325e6f5d
JIWON <26162616@naver.com>
2020-09-24 18:21:04 +0900

    checkbox

--- web_NotHere/NotHereProject/NotHereApp/views.py
+++ web_NotHere/NotHereProject/NotHereApp/views.py
@@ -4,27 +4,48 @@ from .models import StoreDB, ReviewDB
 
 def home(request):
     context={}
+    stores_list =[]
+    stores_result = []
+
+    stores = StoreDB.objects.all()
+
     if request.method == "POST":
-        region = request.POST['region']
-        context['region'] = region
+        # 지역별 분류
+        region = request.POST.getlist('region[]')
+        print(region)
+        # context['region'] = region
 
-        if region == '전체':
-            stores = StoreDB.objects.all()
+        if ('전체' in region) or (region == []) :
+            store_list = stores
+            print("지역전체")
+        else :
+            for reg in region:
+                stores_list.append(stores.filter(addcode=reg))
+            print("지역필터링") 
+
+        # 업종별 분류
+        category = request.POST.getlist('category[]')
+        print(category)
+        # context['category'] = category
+
+        if ('전체' in category) or (category == []) :
+            # stores = StoreDB.objects.all()
+            print("카테고리전체")
         else:
-            stores = StoreDB.objects.filter(addcode=region)
+            for ministore in stores_list:
+                for cate in category:
+                    stores_result.append(ministore.filter(category=cate))
+            print("카테고리필터링")
 
-        category = request.POST['category']
-        context['category'] = category
+        context['stores'] = stores_result
+        print(stores_result)
 
-        if category != '전체':
-            stores = stores.filter(category=category)
     else:
-        stores = StoreDB.objects.all()
-        context['region'] = '전체'
-        context['category'] = '전체'
-
-    context['stores'] = stores
-
+        # context['region'] = '전체'
+        # context['category'] = '전체'
+        print("post없음")
+        context['stores'] = stores
+    
     return render(request, 'home.html', context)
 
 def detail(request, store_pk):
